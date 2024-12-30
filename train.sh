ec70d50947741c46f5d3f4664018a2373aebfde7
Bhavya Bahl <bbahl@google.com>
2024-12-30 22:03:58 +0000

    Update batch size in train command

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export JAX_PLATFORMS=tpu,cpu
+export XLA_USE_SPMD=1
 export PJRT_DEVICE=TPU
 export XLA_IR_DEBUG=1
 export XLA_HLO_DEBUG=1
@@ -7,14 +8,15 @@ export PROFILE_EPOCH=0
 export PROFILE_STEP=3
 export PROFILE_DURATION_MS=40000
 export PROFILE_LOGDIR=gs://bbahl/llama-det/
-python3 examples/pytorch/language-modeling/run_clm.py \
+export LIBTPU_INIT_ARGS="--xla_tpu_sink_dma_done=false"
+python3 pytorch-tpu/examples/pytorch/language-modeling/run_clm.py \
   --dataset_name wikitext \
-  --dataset_config_name wikitext-2-raw-v1 \
-  --per_device_train_batch_size 128 \
+  --dataset_config_name wikitext-103-raw-v1 \
+  --per_device_train_batch_size 1024 \
   --do_train \
   --output_dir /home/$USER/tmp/test-clm \
   --overwrite_output_dir \
-  --config_name llama-2-config.json \
+  --config_name /home/$USER/pytorch-tpu/llama-2-config.json \
   --cache_dir /home/$USER/cache \
   --tokenizer_name hf-internal-testing/llama-tokenizer \
   --block_size 2048 \
